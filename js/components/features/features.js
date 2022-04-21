c1375280a90731d537dfafdef2a24bd89737f012
Rimantas <belauzas@yahoo.com>
2022-04-21 10:37:15 +0300

    Features rafactoring

--- js/components/features/features.js
+++ js/components/features/features.js
@@ -1,15 +1,73 @@
-function renderFeatures(selector, data) {
+function isValidSelector(selector) {
+    if (typeof selector !== 'string') {
+        return [true, 'Selector turi buti stringas'];
+    }
+    if (selector === '') {
+        return [true, 'Selector turi buti ne tuscias stringas'];
+    }
+    return [false, 'OK'];
+}
+
+function isValidData(data) {
+    if (!Array.isArray(data)) {
+        return [true, 'Duomenys turi buti masyve'];
+    }
+    if (data.length === 0) {
+        return [true, 'Duomenyse turi buti bent vienas objektas'];
+    }
+    return [false, 'OK'];
+}
+
+function isValidDataItem(item) {
     const mandatoryObjKeys = ['title', 'desc'];
     const optionalObjKeys = ['icon'];
     const allObjKeys = [...mandatoryObjKeys, ...optionalObjKeys];
     const minKeysCount = mandatoryObjKeys.length;
     const maxKeysCount = minKeysCount + optionalObjKeys.length;
 
-    if (typeof selector !== 'string') {
-        return [true, 'Selector turi buti stringas'];
+    // tikriname, ar tai tikras objektas
+    if (typeof item !== 'object'
+        || item === null
+        || Array.isArray(item)) {
+        return false;
     }
-    if (selector === '') {
-        return [true, 'Selector turi buti ne tuscias stringas'];
+
+    // tikriname, kiek raktazodziu yra objekte
+    const keys = Object.keys(item);
+    if (keys.length < minKeysCount || keys.length > maxKeysCount) {
+        return false;
+    }
+
+    // ieskom ar bent vienas objekte esantis key neturetu jam priklausyti
+    let correctObj = true;
+    for (const key of keys) {
+        if (!allObjKeys.includes(key)) {
+            correctObj = false;
+            break;
+        }
+    }
+
+    // radom netinkama key
+    if (!correctObj) {
+        return false;
+    }
+
+    // ar objekte yra visi privalomi raktazodziai
+    if (typeof item.title !== 'string'
+        || item.title.trim() === '') {
+        return false;
+    }
+    if (typeof item.desc !== 'string'
+        || item.desc.trim() === '') {
+        return false;
+    }
+    return true;
+}
+
+function renderFeatures(selector, data) {
+    const selectorRes = isValidSelector(selector);
+    if (selectorRes[0]) {
+        return selectorRes;
     }
 
     const DOM = document.querySelector(selector);
@@ -17,50 +75,15 @@ function renderFeatures(selector, data) {
         return [true, 'Pagal pateikta selector nepavyko rasti elemento'];
     }
 
-    if (!Array.isArray(data)) {
-        return [true, 'Duomenys turi buti masyve'];
-    }
-    if (data.length === 0) {
-        return [true, 'Duomenyse turi buti bent vienas objektas'];
+    const dataRes = isValidData(data);
+    if (dataRes[0]) {
+        return dataRes;
     }
 
     let HTML = '';
 
     for (const item of data) {
-        // tikriname, ar tai tikras objektas
-        if (typeof item !== 'object'
-            || item === null
-            || Array.isArray(item)) {
-            continue;
-        }
-
-        // tikriname, kiek raktazodziu yra objekte
-        const keys = Object.keys(item);
-        if (keys.length < minKeysCount || keys.length > maxKeysCount) {
-            continue;
-        }
-
-        // ieskom ar bent vienas objekte esantis key neturetu jam priklausyti
-        let correctObj = true;
-        for (const key of keys) {
-            if (!allObjKeys.includes(key)) {
-                correctObj = false;
-                break;
-            }
-        }
-
-        // radom netinkama key
-        if (!correctObj) {
-            continue;
-        }
-
-        // ar objekte yra visi privalomi raktazodziai
-        if (typeof item.title !== 'string'
-            || item.title.trim() === '') {
-            continue;
-        }
-        if (typeof item.desc !== 'string'
-            || item.desc.trim() === '') {
+        if (!isValidDataItem(item)) {
             continue;
         }
 
